397b44c4f020316ec5fb16ffef659e620a3a299b
Anna Anello <125416908+Ollena5@users.noreply.github.com>
2024-05-03 15:59:12 +0000

    is it working?????

--- Homework6/mirror.js
+++ Homework6/mirror.js
@@ -1,15 +1,16 @@
-mirror=function(x){
-    if (! Array.isArray(x)){
-        console.log("A")
-        return x
+function mirror(x) {
+    if (!Array.isArray(x)) {
+        return x;
     }
-    if(x.length==0){
-        console.log("B")
-        return []
+    if (x.length === 0) {
+        return [];
     }
-    console.log("C")
-    return mirror(x.slice(1)).concat([mirror(x[0])])
+    return x.slice(1).reduceRight((acc, curr) => {
+        acc.push(mirror(curr));
+        return acc;
+    }, []).concat([mirror(x[0])]);
 }
-console.log(mirror(1));
-console.log(mirror([]));
-console.log(mirror([1]));
+
+console.log(mirror([1, 2, [3, [4, 5], 6]])); // [[6,[5,4],3],2,1]
+console.log(mirror([])); // []
+console.log(mirror([1])); // [1]
